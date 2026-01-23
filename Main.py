08c60838b50d6d90fa8ce9ad371a3ff32c02cf8d
vl05953 <adam.szilagyi@ksh.hu>
2026-01-23 09:58:08 +0100

    raise waiting time to 12 secs

--- Main.py
+++ Main.py
@@ -46,7 +46,7 @@ async def run_playwright():
       user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     )
     
-    context.set_default_timeout(10000)
+    context.set_default_timeout(12000)
     page = await context.new_page() # browser.
     await page.goto("https://port.hu/programkereso/zene") # 
     await page.wait_for_timeout(2000)
